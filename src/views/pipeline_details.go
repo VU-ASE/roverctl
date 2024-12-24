5d3e73e4189b9c34b993ccd517bb02ecc7a760b6
Elias Groot <github@ielaajez.com>
2024-12-24 15:12:07 +0100

    feat: show correct service details (including build date)

--- src/views/pipeline_details.go
+++ src/views/pipeline_details.go
@@ -1,8 +1,15 @@
 package views
 
 import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/VU-ASE/rover/src/openapi"
 	"github.com/VU-ASE/rover/src/state"
 	"github.com/VU-ASE/rover/src/style"
+	"github.com/VU-ASE/rover/src/tui"
+	"github.com/VU-ASE/rover/src/utils"
 	"github.com/charmbracelet/bubbles/help"
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
@@ -45,6 +52,7 @@ type PipelineDetailsPage struct {
 	help    help.Model
 	spinner spinner.Model
 	service PipelineOverviewServiceInfo
+	details tui.Action[openapi.ServicesAuthorServiceVersionGet200Response]
 }
 
 func NewPipelineDetailsPage(s PipelineOverviewServiceInfo) PipelineDetailsPage {
@@ -52,6 +60,7 @@ func NewPipelineDetailsPage(s PipelineOverviewServiceInfo) PipelineDetailsPage {
 		spinner: spinner.New(),
 		help:    help.New(),
 		service: s,
+		details: tui.NewAction[openapi.ServicesAuthorServiceVersionGet200Response]("details"),
 	}
 }
 
@@ -65,13 +74,18 @@ func (m PipelineDetailsPage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case spinner.TickMsg:
 		m.spinner, cmd = m.spinner.Update(msg)
 		return m, cmd
+	case tui.ActionInit[openapi.ServicesAuthorServiceVersionGet200Response]:
+		m.details.ProcessInit(msg)
+		return m, nil
+	case tui.ActionResult[openapi.ServicesAuthorServiceVersionGet200Response]:
+		m.details.ProcessResult(msg)
+		return m, nil
 	case tea.KeyMsg:
 		switch {
 		case key.Matches(msg, pipelineDetailsKeysRegular.Quit):
 			return m, tea.Quit
 		case key.Matches(msg, pipelineDetailsKeysRegular.Retry):
-			// todo:
-			return m, nil
+			return m, m.fetchVersionDetails()
 		case key.Matches(msg, pipelineDetailsKeysRegular.Confirm):
 			// todo:
 			return m, nil
@@ -82,54 +96,93 @@ func (m PipelineDetailsPage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m PipelineDetailsPage) Init() tea.Cmd {
-	return tea.Batch(m.spinner.Tick)
+	return tea.Batch(m.spinner.Tick, m.fetchVersionDetails())
 }
 
 func (m PipelineDetailsPage) View() string {
 	s := style.Title.Render("Details") + "\n\n"
 
-	// Calculate column width (subtract padding and borders)
-	columnWidth := (state.Get().WindowWidth - 4 - 6) / 3 // Adjust for padding and borders
+	if m.details.IsError() {
+		s += style.Error.Render("Error fetching service details: ") + m.details.Error.Error()
+	} else if m.details.HasData() {
+		service := *m.details.Data
 
-	// Define styles for each column
-	columnStyle := lipgloss.NewStyle().
-		Width(columnWidth)
+		// Calculate column width (subtract padding and borders)
+		columnWidth := (state.Get().WindowWidth - 4 - 6) / 3 // Adjust for padding and borders
+
+		// Define styles for each column
+		columnStyle := lipgloss.NewStyle().
+			Width(columnWidth)
 
 		// About
-	about := style.Gray.Render("Service: ") + m.service.Name + "\n"
-	about += style.Gray.Render("Author: ") + m.service.Author + "\n"
-	about += style.Gray.Render("Version: ") + m.service.Version + "\n"
+		about := style.Gray.Render("Service: ") + m.service.Name + "\n"
+		about += style.Gray.Render("Author: ") + m.service.Author + "\n"
+		about += style.Gray.Render("Version: ") + m.service.Version + "\n"
+		about += style.Gray.Render("Last build: ")
+		if service.BuiltAt != nil {
+			about += time.Unix(*service.BuiltAt/1000, 0).Format("2006-01-02 15:04:05")
+		} else {
+			about += "not built yet"
+		}
+		about += "\n"
 
-	inputs := ""
-	if len(m.service.Configuration.Inputs) <= 0 {
-		inputs += style.Gray.Render("This service does not depend on streams from other services")
-	} else {
-		inputs += style.Gray.Render("Inputs: ") + "\n"
+		inputs := ""
+		if len(service.Inputs) <= 0 {
+			inputs += style.Gray.Render("This service does not depend on streams from other services")
+		} else {
+			inputs += style.Gray.Render("Inputs: ") + "\n"
 
-		for _, i := range m.service.Configuration.Inputs {
-			for _, s := range i.Streams {
-				inputs += style.Gray.Render("- ") + s + style.Gray.Render(" from ") + i.Service + "\n"
+			for _, i := range m.service.Configuration.Inputs {
+				for _, s := range i.Streams {
+					inputs += style.Gray.Render("- ") + s + style.Gray.Render(" from ") + i.Service + "\n"
+				}
 			}
 		}
-	}
 
-	outputs := ""
-	if len(m.service.Configuration.Outputs) <= 0 {
-		outputs += style.Gray.Render("This service does not provide streams to other services")
-	} else {
-		outputs += style.Gray.Render("Outputs: ") + "\n"
+		outputs := ""
+		if len(m.service.Configuration.Outputs) <= 0 {
+			outputs += style.Gray.Render("This service does not provide streams to other services")
+		} else {
+			outputs += style.Gray.Render("Outputs: ") + "\n"
 
-		for _, o := range m.service.Configuration.Outputs {
-			outputs += style.Gray.Render("- ") + o + "\n"
+			for _, o := range m.service.Configuration.Outputs {
+				outputs += style.Gray.Render("- ") + o + "\n"
+			}
 		}
+
+		row := lipgloss.JoinHorizontal(lipgloss.Top,
+			columnStyle.Render(about),
+			columnStyle.Render(inputs),
+			columnStyle.Render(outputs),
+		)
+
+		s += row
+	} else {
+		s += m.spinner.View() + " Fetching service details..."
 	}
 
-	row := lipgloss.JoinHorizontal(lipgloss.Top,
-		columnStyle.Render(about),
-		columnStyle.Render(inputs),
-		columnStyle.Render(outputs),
-	)
+	return s + "\n" + m.help.View(pipelineDetailsKeysRegular)
+}
 
-	return s + row + "\n" + m.help.View(pipelineDetailsKeysRegular)
+func (m PipelineDetailsPage) fetchVersionDetails() tea.Cmd {
+	return tui.PerformAction(&m.details, func() (*openapi.ServicesAuthorServiceVersionGet200Response, error) {
+		remote := state.Get().RoverConnections.GetActive()
+		if remote == nil {
+			return nil, fmt.Errorf("No active rover connection")
+		}
 
+		api := remote.ToApiClient()
+		res, htt, err := api.ServicesAPI.ServicesAuthorServiceVersionGet(
+			context.Background(),
+			m.service.Author,
+			m.service.Name,
+			m.service.Version,
+		).Execute()
+
+		if err != nil && htt != nil {
+			return nil, utils.ParseHTTPError(err, htt)
+		}
+
+		return res, err
+	})
 }
